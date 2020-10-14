b719ae27f18a39982b1e6bb24444c63adee78dae
NergzMohammed <72860630+NergzMohammed@users.noreply.github.com>
2020-10-14 12:36:24 +0100

    Add files via upload

---
+++ assignment_2/Question1.java
@@ -0,0 +1,76 @@
+package assignment_2;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+	
+	
+	    //Question 1
+	
+		int count= 298;
+		System.out.println(count);
+		
+		//Question 2
+		
+		String message= "Super excited";
+		
+		System.out.println("message= "  + message);
+		
+		//Question 3
+		
+		int num=1876, double cost=65496965;
+		
+		//Question 4
+		
+		String word="Marvelous";
+		
+		System.out.println("Today's Word-Of-The-Day is = " + word);
+				
+		//Question 5
+		
+		int num=55; 
+        int num2=123;
+		System.out.println("first is= "+ num + " second= " + num2);
+		
+		//Question 6
+		String name1="Bob", name2="Jill", name3="Bethy", name4="Herb", name5="James";
+
+		System.out.println("Bob,"+"Jill,"+"Bethy,"+"Herb,"+"James.");
+		
+		
+		//Question7
+		
+		int i= 20;
+		double f=33.5;
+		
+		System.out.println("i= " + i +" f= "+ f);
+		 
+		
+		//Question 8
+		
+		String lan1="Java", lan2="SQL"; 
+		System.out.println("I will learn "+ "Java " +"and "+"SQL "+"at CybertekSchool.");
+		
+		//Question 9
+		
+		byte steps=100;
+	    short miles= 5000;
+        int count1= 1000000;
+        long population=343445667;
+        
+       // Question 10
+       int hour= 05,minute=26,second=47;
+		
+	    String amOrPm= "AM";
+	    
+	    System.out.println("05:"+ "26:"+"33 "+"AM");
+		
+		
+       		
+		
+		
+		
+		
+	}
+
+}
